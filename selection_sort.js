9b6cc972750dd27ff0832e6ffe472459f3dcf630
handipriyono <handimacbook@gmail.com>
2018-05-01 19:26:34 +0700

    add selection sort

---
+++ selection_sort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    var min = i; // first init
+
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j; //exchange
+      }
+    }
+    if (min !== i) {
+      var change = arr[i];
+      arr[i] = arr[min];
+      arr[min] = change;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([33, 2, 52, 106, 73])); // [2, 33, 52, 73, 106]
+console.log(selectionSort([13, 5, 22, 99, 11])); // [5, 11, 13, 22, 99]
